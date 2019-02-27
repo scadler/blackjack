9ec4f8d2bae5f37db122cc27ccc5fe2b28f000ef
Scott Adler <anorangepi@gmail.com>
2019-02-27 01:50:34 +0000

    finished day one tasks

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
+  $.ajax({
+    url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+    method: "GET",
+    success: function(response) {
+        $(".text-center").append(`<img src="${response.data[0].images.original.url}">`)
+        console.log(`${response.data[0].images.original.url}`)
+  },
+});
   
 });
 
