6c7622c00a37cdafaff8355aaea57f4a4c1f5a3d
Scott Adler <anorangepi@gmail.com>
2019-03-06 01:47:59 +0000

    added multiple gifs

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  var input = "https://api.giphy.com/v1/gifs/search?q=" + $("#search-term").val() + "&rating=pg&api_key=dc6zaTOxFJmzC";
+  var input = "https://api.giphy.com/v1/gifs/search?q=" + $("#search-term").val() + "&rating=pg&api_key=dc6zaTOxFJmzC&limit=3";
   console.log(input);
   $.ajax({
     url: input,
@@ -12,8 +12,12 @@ $("#search-button").click(function(){
     success: function(response) {
         $(".text-center").append(`<img src="${response.data[0].images.original.url}">`);
         console.log(`${response.data[0].images.original.url}`);
-        $(".text-center").replaceWith("<img class='text-center' src='" + response.data[0].images.original.url + "'>");
-  },
+        $(".text-center").html("");
+        response.data.forEach(function(gifs){
+        $(".text-center").append("<img class='text-center' src='" + gifs.images.original.url + "'>" );
+        });
+        },
+
 });
   
 });
