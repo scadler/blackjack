de9ecbcb08c8a11719a048cecb272328b847b80b
Scott Adler <anorangepi@gmail.com>
2019-03-06 01:06:12 +0000

    working search

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@
 /* global $ */
 
 $("#search-button").click(function(){
+  var input = "https://api.giphy.com/v1/gifs/search?q=" + $("#search-term").val() + "&rating=pg&api_key=dc6zaTOxFJmzC";
+  console.log(input);
   $.ajax({
-    url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+    url: input,
     method: "GET",
     success: function(response) {
-        $(".text-center").append(`<img src="${response.data[0].images.original.url}">`)
-        console.log(`${response.data[0].images.original.url}`)
+        $(".text-center").append(`<img src="${response.data[0].images.original.url}">`);
+        console.log(`${response.data[0].images.original.url}`);
+        $(".text-center").replaceWith("<img class='text-center' src='" + response.data[0].images.original.url + "'>");
   },
 });
   
