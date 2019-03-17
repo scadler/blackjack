5a74dfde7e4afab6e1e35cea763ed060d6b8938b
Scott Adler <anorangepi@gmail.com>
2019-03-17 05:23:47 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@
 /* global $ */
 
 setInterval(function(){ 
-    var red = 
-    $("body").css("background-color", "#" + red + blue + green )   
-}, 50);
+    var red = $("#inputRed").val();
+    var redHex = parseInt(red, 10).toString(16);
+    console.log(redHex);
+    var blue = $("#inputBlue").val();
+    var blueHex = parseInt(blue, 10).toString(16);
+    console.log(redHex);
+    var green = $("#inputGreen").val();
+    var greenHex = parseInt(green, 10).toString(16);
+    console.log(redHex);
+    $("body").css("background-color", "#" + redHex + greenHex + blueHex );   
+}, 15);
