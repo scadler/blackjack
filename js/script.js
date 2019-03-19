5c408cf60ec9b91b5baf9833dabb9aa85d67a795
Scott Adler <anorangepi@gmail.com>
2019-03-19 01:41:07 +0000

    added player hand

--- js/script.js
+++ js/script.js
@@ -53,8 +53,6 @@ $("#deal").click(function(){
     //hit
     
     $("#hit").click(function(){
-        var counterMain = 1
-        counterMain = counterMain+1;
         $("#aceFlipD").hide();
         cardC=0;
         cardD=0;
@@ -77,7 +75,46 @@ $("#deal").click(function(){
                 $("#aceFlipC").show();
                 aces = aces+1;
             }
-            
+               if(cardC === 1){
+                    faceC = "Ace of Clubs";
+                }
+                else if(cardC === 2){
+                    faceC = "Two of Clubs";
+                }
+                else if(cardC === 3){
+                    faceC = "Three of Clubs";
+                }
+                else if(cardC === 4){
+                    faceC = "Four of Clubs";
+                }
+                else if(cardC === 5){
+                    faceC = "Five of Clubs";
+                }
+                else if(cardC === 6){
+                    faceC = "Six of Clubs";
+                }
+                else if(cardC === 7){
+                    faceC = "Seven of Clubs";
+                }
+                else if(cardC === 8){
+                   faceC = "Eight of Clubs";
+                }
+                else if(cardC === 9){
+                    faceC = "Nine of Clubs";
+                }
+                else if(cardC === 10){
+                    faceC = "Ten of Clubs";
+                }
+                else if(cardC === 11){
+                    faceC = "Jack of Clubs";
+                }
+                else if(cardC === 12){
+                    faceC = "Queen of Clubs";
+                }
+                else if(cardC === 13){
+                    faceC = "King ofClubs";
+                } 
+                $("#userHand").text(faceA +", "+ faceB +", "+ faceC);
         
         
             $("#aceFlipA").click(function(){
@@ -123,8 +160,6 @@ $("#deal").click(function(){
             $("#userScore").text(cardA + cardB + cardC);
         }
         $("#stand").click(function(){
-        
-        if(counterMain % 2 === 0){
             if(hitTwo === false){
                 if(gameOver === false){
                 if(compDraw === true){
@@ -139,33 +174,25 @@ $("#deal").click(function(){
                 }
             }
                 compDraw = false;
-                hand = cardA+cardB+cardC;
-                console.log(hand);
-                console.log(hand +" userScore");
-                console.log(compCard) +" compScore";
-                if(hand===21 && compCard!==21){
-                    $("#winner").text("User");
-                }
-                if(hand < 22 && compCard < 22){
-                        if(hand > compCard){
-                            $("#winner").text("User");
-                        }  
-                        else if(hand < compCard){
-                            $("#winner").text("Computer");
-                        }  
-                }
-                else if(compCard >= 22 && hand < 22){
-                    $("#winner").text("User");
-                }
-                else if(hand===compCard){
-                        $("#winner").text("Computer");
-                    }
-                else{
-                    $("#winner").text("Computer");
-                }
-            }
-
-        });
+    //            hand = cardA+cardB+cardC;
+      //          console.log(hand);
+        //        if(hand < 22 && compCard < 22){
+          //              if(hand > compCard){
+            //                $("#winner").text("User");
+              //          }  
+                //        if(hand < compCard){
+                  //          $("#winner").text("Computer");
+                    //    }  
+//                }
+  //              if(compCard >= 22 && hand < 22){
+    //                $("#winner").text("User");
+      //          }
+        //        else{
+          //          $("#winner").text("Computer");
+            //    }
+//            }
+  //          }
+});
         $("#hit").click(function(){
             hitTwo=true;
             if(counterHit % 2 === 1  && counterHit === 3){
@@ -188,7 +215,46 @@ $("#deal").click(function(){
                $("#aceFlipD").show();
                 aces = aces+1;
             }
-            
+            if(cardD === 1){
+                    faceD = "Ace of Diamonds";
+                }
+                else if(cardD === 2){
+                    faceD = "Two of Diamonds";
+                }
+                else if(cardD === 3){
+                    faceD = "Three of Diamonds";
+                }
+                else if(cardD === 4){
+                    faceD = "Four of Diamonds";
+                }
+                else if(cardD === 5){
+                    faceD = "Five of Diamonds";
+                }
+                else if(cardD === 6){
+                    faceD = "Six of Diamonds";
+                }
+                else if(cardD === 7){
+                    faceD = "Seven of Diamonds";
+                }
+                else if(cardD === 8){
+                   faceD = "Eight of Diamonds";
+                }
+                else if(cardD === 9){
+                    faceD = "Nine of Diamonds";
+                }
+                else if(cardD === 10){
+                    faceD = "Ten of Diamonds";
+                }
+                else if(cardD === 11){
+                    faceD = "Jack of Diamonds";
+                }
+                else if(cardD === 12){
+                    faceD = "Queen of Diamonds";
+                }
+                else if(cardD === 13){
+                    faceD = "King of Diamonds";
+                } 
+                $("#userHand").text(faceA +", "+ faceB +", "+ faceC +", "+ faceD);
             
             $("#aceFlipA").click(function(){
                 counterA=counterA+1;
@@ -237,8 +303,6 @@ $("#deal").click(function(){
             });
             $("#userScore").text(cardA + cardB + cardC + cardD);
             $("#stand").click(function(){
-        
-            if(counterMain % 2 === 1){
             if(hitTwo === true){
                 if(gameOver === false){
                 if(compDraw === true){
@@ -250,33 +314,24 @@ $("#deal").click(function(){
                             }
                     $("#compScore").text(compCard);
                     }
-        
-              hand = cardA+cardB+cardC+cardD;
+    //                hand = $("#userScore").val();
                     compDraw = false;
-                    if(hand===21 && compCard!==21){
-                    $("#winner").text("User");
-                }
-                    if(hand===compCard){
-                        $("#winner").text("Computer");
-                    }
-                if(hand < 22 && compCard < 22){
-                        if(hand > compCard){
-                            $("#winner").text("User");
-                        }  
-                        if(hand < compCard){
-                            $("#winner").text("Computer");
-                        }  
-                }
-                if(compCard >= 22 && hand < 22){
-                    $("#winner").text("User");
-                }
-                else{
-                    $("#winner").text("Computer");
-                }
+        //        if(hand < 22 && compCard < 22){
+          //              if(hand > compCard){
+            //                $("#winner").text("User");
+              //          }  
+                //        if(hand < compCard){
+                  //          $("#winner").text("Computer");
+                    //    }  
+//                }
+  //              if(compCard >= 22 && hand < 22){
+    //                $("#winner").text("User");
+      //          }
+        //        else{
+          //          $("#winner").text("Computer");
+            //    }
                }
             }
-            }
-            
             });
             }
     });
@@ -293,7 +348,7 @@ $("#deal").click(function(){
         else if( counterA % 2 === 1){
             cardA=1;
         }
-        hand=cardA+cardB+cardC+cardD;
+        hand=cardA+cardB;
         $("#userScore").text(hand);
         }
     });
@@ -327,6 +382,7 @@ $("#deal").click(function(){
     $("#userScore").text(hand);
     
     //computer logic
+    
     $("#stand").click(function(){
         console.log("stand");
             if(gameOver === false){
@@ -340,29 +396,111 @@ $("#deal").click(function(){
                     $("#compScore").text(compCard);
                     }
                 compDraw = false;
-                if(hand===21 && compCard!==21){
-                    $("#winner").text("User");
-                }
-               if(hand < 22 && compCard < 22){
-                       if(hand > compCard){
-                            $("#winner").text("User");
-                        }  
-                        if(hand < compCard){
-                            $("#winner").text("Computer");
-                        }  
-                }
-                if(compCard >= 22 && hand < 22){
-                    $("#winner").text("User");
-                }
-                if(hand===compCard){
-                        $("#winner").text("Computer");
-                    }
-                else{
-                    $("#winner").text("Computer");
-                }
-            }
-    });
-     
+//                if(hand < 22 && compCard < 22){
+//                       if(hand > compCard){
+//                            $("#winner").text("User");
+//                        }  
+//                        if(hand < compCard){
+//                            $("#winner").text("Computer");
+//                        }  
+//                }
+//                if(compCard >= 22 && hand < 22){
+//                    $("#winner").text("User");
+//                }
+ //                else{
+//                    $("#winner").text("Computer");
+//                }
+//}
+//});
+}
+});
+    var faceA = 0;
+    var faceB = 0;
+    var faceC = 0;
+    var faceD = 0;
+   if(cardA === 1){
+    faceA = "Ace of Spades";
+}
+else if(cardA === 2){
+    faceA = "Two of Spades";
+}
+else if(cardA === 3){
+    faceA = "Three of Spades";
+}
+else if(cardA === 4){
+    faceA = "Four of Spades";
+}
+else if(cardA === 5){
+    faceA = "Five of Spades";
+}
+else if(cardA === 6){
+    faceA = "Six of Spades";
+}
+else if(cardA === 7){
+    faceA = "Seven of Spades";
+}
+else if(cardA === 8){
+    faceA = "Eight of Spades";
+}
+else if(cardA === 9){
+    faceA = "Nine of Spades";
+}
+else if(cardA === 10){
+    faceA = "Ten of Spades";
+}
+else if(cardA === 11){
+    faceA = "Jack of Spades";
+}
+else if(cardA === 12){
+    faceA = "Queen of Spades";
+}
+else if(cardA === 13){
+    faceA = "King of Spades";
+} 
+
+//faceB
+   if(cardB === 1){
+    faceB = "Ace of Hearts";
+}
+else if(cardB === 2){
+    faceB = "Two of Hearts";
+}
+else if(cardB === 3){
+    faceB = "Three of Hearts";
+}
+else if(cardB === 4){
+    faceB = "Four of Hearts";
+}
+else if(cardB === 5){
+    faceB = "Five of Hearts";
+}
+else if(cardB === 6){
+    faceB = "Six of Hearts";
+}
+else if(cardB === 7){
+    faceB = "Seven of Hearts";
+}
+else if(cardB === 8){
+    faceB = "Eight of Hearts";
+}
+else if(cardB === 9){
+    faceB = "Nine of Hearts";
+}
+else if(cardB === 10){
+    faceB = "Ten of Hearts";
+}
+else if(cardB === 11){
+    faceB = "Jack of Hearts";
+}
+else if(cardB === 12){
+    faceB = "Queen of Hearts";
+}
+else if(cardB === 13){
+    faceB = "King of Hearts";
+} 
+$("#userHand").text(faceA +", "+ faceB);
+
+
 });
 $("#dark").click(function(){
     $(".score").css("color", "#f9f9f9");
@@ -385,3 +523,4 @@ $("#light").click(function(){
     $(".lightButtons").css("background-color", "#f2f2f2");
     $(".lightButtons").css("color", "#777777");
 });
+
